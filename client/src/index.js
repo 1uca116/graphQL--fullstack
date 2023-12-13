dc4e4ca5e70454b36d220fcf7e4800289ca617f1
luciabakirova <l.bakirova116@gmail.com>
2023-12-13 16:55:23 +0300

    Lift-off I: Basics

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client'
+import ReactDOM from 'react-dom';
+import GlobalStyles from './styles';
+import Pages from './pages';
 
-const client = new ApolloClient({
-    uri: 'http://localhost:8086/graphql',
-    cache: new InMemoryCache()
-})
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>
+ReactDOM.render(
+  <React.StrictMode>
+    <GlobalStyles />
+    <Pages />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
